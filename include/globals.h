c731d2c2a671e6d9c2c8fe5df733f51c09ed3d2c
OrientalMagic12 <ericjiang2008@hotmail.com>
2023-10-08 01:49:38 +0800

    Update globals.h

--- include/globals.h
+++ include/globals.h
@@ -7,12 +7,14 @@ extern::pros::Controller controller;
 extern::pros::Motor leftWheel1;
 extern::pros::Motor leftWheel2;
 extern::pros::Motor leftWheel3;
-extern::pros::Motor_Group leftWheels;
+extern::pros::Motor_Group leftWheels3;
+extern::pros::Motor_Group leftWheels2;
 
 extern::pros::Motor rightWheel1;
 extern::pros::Motor rightWheel2;
 extern::pros::Motor rightWheel3;
-extern::pros::Motor_Group rightWheels;
+extern::pros::Motor_Group rightWheels3;
+extern::pros::Motor_Group rightWheels2;
 
 //catapult
 extern::pros::Motor catapultMotor;
@@ -26,3 +28,6 @@ extern::pros::Rotation odometryWheel;
 //wings
 extern::pros::ADIDigitalOut leftWing;
 extern::pros::ADIDigitalOut rightWing;
+
+//pto
+extern::pros::ADIDigitalOut pto;
